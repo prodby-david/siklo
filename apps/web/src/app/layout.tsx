78330390d3eb85512007d7e8f06ab640e3b4525c
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-06-17 07:03:16 +0800

    fix: add toast and inter font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import Footer from "@/shared/components/footer/Footer";
 import ThemeProvider from "@/shared/components/theme/ThemeProvider";
 import "./globals.css";
+import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Siklo",
@@ -15,11 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-full flex flex-col">
+      <body className={`${inter.className} min-h-full flex flex-col`}>
         <ThemeProvider>
           {children}
           <Footer />
         </ThemeProvider>
+        <Toaster
+          richColors
+          toastOptions={{
+            classNames: {
+              toast: "bg-brand-accent text-foreground border-neutral-border",
+              success: "text-success bg-success-bg border-success/20",
+              error: "text-danger bg-danger-bg border-color-danger-border",
+            },
+          }}
+          position="bottom-right"
+        />
       </body>
     </html>
   );
