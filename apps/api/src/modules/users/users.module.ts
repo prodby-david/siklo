09348eaa0683c24a00d74feafe162aa1f81beefc
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-06-17 07:55:23 +0800

    feat: add user repo provider

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthModule } from '../auth/auth.module';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [AuthModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
