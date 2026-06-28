b960564af70f5b33884d9da972ed7480cd78323c
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-06-28 10:56:16 +0800

    fix: add auth guard and decorator

--- apps/api/src/modules/users/users.controller.ts
+++ apps/api/src/modules/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { userSchema, type CreateUserDTO } from './schema/user.schema';
 import { ZodValidationPipe } from '@/commons/pipes/zod-validation.pipes';
+import { JwtAuthGuard } from '@/commons/guards/jwt-auth';
+import { CurrentUser } from '@/commons/decorators/current-user.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +14,11 @@ export class UsersController {
     @Body(new ZodValidationPipe(userSchema)) data: CreateUserDTO,
   ) {
     return this.usersService.createUser(data);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getCurrentUserName(@CurrentUser('sub') userId: string) {
+    return this.usersService.getCurrentUserName(userId);
   }
 }
