e5c4cb5dfc9034e19bf00829c0753733610cea89
John David Gaspar <johndavidgaspar1107@gmail.com>
2026-06-28 08:32:41 +0800

    fix: add necessary schema field and types

--- apps/api/src/modules/groups/schema/create-group.schema.ts
+++ apps/api/src/modules/groups/schema/create-group.schema.ts
@@ -2,11 +2,28 @@ import { z } from 'zod';
 
 export const createGroupWithMemberSchema = z.object({
   name: z.string().min(3),
-  description: z.string().min(3),
+  description: z.string().min(3).optional(),
   contributionAmount: z.coerce.number().min(1),
-  startDate: z.string(),
-  groupId: z.string(),
-  userId: z.string(),
+  cycleDuration: z.coerce.number().min(1),
+  billingCycle: z.enum([
+    'DAILY',
+    'WEEKLY',
+    'MONTHLY',
+    'BIMONTHLY',
+    'QUARTERLY',
+  ]),
+  startDate: z.coerce.date(),
+  inviteCode: z.string().length(6),
+  organizerId: z.string(),
+  maxMembers: z.coerce
+    .number()
+    .min(3, 'Minimum number of members should be at least 3'),
+});
+
+export const createGroupSchema = createGroupWithMemberSchema.omit({
+  inviteCode: true,
+  organizerId: true,
 });
 
 export type CreateGroupDTO = z.infer<typeof createGroupWithMemberSchema>;
+export type CreateGroupData = z.infer<typeof createGroupSchema>;
